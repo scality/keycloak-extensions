441c2b40565ef3f751e40db2a512f04d7dbf879b
Jean-Baptiste WATENBERG <jean-baptiste.watenberg@scality.com>
2023-12-22 16:52:28 +0100

    IT WORKS

--- src/main/java/com/scality/keycloak/truststore/TruststoreAdminResource.java
+++ src/main/java/com/scality/keycloak/truststore/TruststoreAdminResource.java
@@ -10,12 +10,15 @@ import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 import org.jboss.logging.Logger;
 import org.jboss.resteasy.annotations.cache.NoCache;
 import org.keycloak.models.KeycloakSession;
-import org.keycloak.representations.idm.CertificateRepresentation;
 import org.keycloak.services.resources.KeycloakOpenAPI;
 import org.keycloak.services.resources.admin.permissions.AdminPermissionEvaluator;
-import org.wildfly.security.x500.cert.X509CertificateBuilder;
 
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.PATCH;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
@@ -40,9 +43,57 @@ public class TruststoreAdminResource {
     public Stream<CertificateRepresentation> getCertificates() {
         auth.realm().requireManageRealm();
 
-        logger.info("getCertificates");
+        return Stream.of(session.getProvider(CertificateTruststoreProvider.class).getCertificates());
+    }
 
-        return Stream.of(new CertificateRepresentation());
+    @POST
+    @NoCache
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Tag(name = OPEN_API_TAG)
+    @Operation(summary = "Add certificate Adds a trusted certificate.")
+    public void addCertificate(CertificateRepresentation certificate) throws CertificateException {
+        auth.realm().requireManageRealm();
+
+        session.getProvider(CertificateTruststoreProvider.class).addCertificate(certificate.alias(),
+                certificate.certificate());
+    }
+
+    @Path("{alias}")
+    @GET
+    @NoCache
+    @Produces(MediaType.APPLICATION_JSON)
+    @Tag(name = OPEN_API_TAG)
+    @Operation(summary = "Get certificate Returns a trusted certificate.")
+    public CertificateRepresentation getCertificate(String alias) {
+        auth.realm().requireManageRealm();
+
+        return session.getProvider(CertificateTruststoreProvider.class).getCertificate(alias);
+    }
+
+    @Path("{alias}")
+    @PATCH
+    @NoCache
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Tag(name = OPEN_API_TAG)
+    @Operation(summary = "Update certificate Updates a trusted certificate.")
+    public void updateCertificate(String alias, CertificateRepresentation certificate) throws CertificateException {
+        auth.realm().requireManageRealm();
+
+        session.getProvider(CertificateTruststoreProvider.class).updateCertificate(alias, certificate.certificate());
+    }
+
+    @Path("{alias}")
+    @DELETE
+    @NoCache
+    @Produces(MediaType.APPLICATION_JSON)
+    @Tag(name = OPEN_API_TAG)
+    @Operation(summary = "Remove certificate Removes a trusted certificate.")
+    public void removeCertificate(String alias) {
+        auth.realm().requireManageRealm();
+
+        session.getProvider(CertificateTruststoreProvider.class).removeCertificate(alias);
     }
 
 }
---
+++ src/test/java/com/scality/keycloak/FullImageName.java
@@ -0,0 +1,66 @@
+package com.scality.keycloak;
+
+import org.testcontainers.images.ImagePullPolicy;
+import org.testcontainers.images.PullPolicy;
+
+import dasniko.testcontainers.keycloak.KeycloakContainer;
+import static java.lang.module.ModuleDescriptor.Version;
+
+public class FullImageName {
+    enum Distribution {
+        quarkus
+    }
+
+    private static final Distribution KEYCLOAK_DIST = Distribution.valueOf(
+            System.getProperty("keycloak.dist", Distribution.quarkus.name()));
+
+    private static final String LATEST_VERSION = "latest";
+    private static final String NIGHTLY_VERSION = "nightly";
+    private static final String KEYCLOAK_VERSION = System.getProperty("keycloak.version", LATEST_VERSION);
+
+    static String get() {
+        String imageName = "keycloak";
+
+        if (!isNightlyVersion()) {
+            if (!isLatestVersion()) {
+                if (getParsedVersion().compareTo(Version.parse("17")) < 0) {
+                    if (Distribution.quarkus.equals(KEYCLOAK_DIST)) {
+                        imageName = "keycloak-x";
+                    }
+                }
+            }
+        }
+
+        return "quay.io/keycloak/" + imageName + ":" + KEYCLOAK_VERSION;
+    }
+
+    static Boolean isNightlyVersion() {
+        return NIGHTLY_VERSION.equalsIgnoreCase(KEYCLOAK_VERSION);
+    }
+
+    static Boolean isLatestVersion() {
+        return LATEST_VERSION.equalsIgnoreCase(KEYCLOAK_VERSION);
+    }
+
+    static Version getParsedVersion() {
+        if (isLatestVersion()) {
+            return null;
+        }
+        return Version.parse(KEYCLOAK_VERSION);
+    }
+
+    static Distribution getDistribution() {
+        return KEYCLOAK_DIST;
+    }
+
+    static KeycloakContainer createContainer() {
+        String fullImage = FullImageName.get();
+        ImagePullPolicy pullPolicy = PullPolicy.defaultPolicy();
+        if (isLatestVersion() || isNightlyVersion()) {
+            pullPolicy = PullPolicy.alwaysPull();
+        }
+        return new KeycloakContainer(fullImage)
+                .withImagePullPolicy(pullPolicy);
+    }
+
+}
