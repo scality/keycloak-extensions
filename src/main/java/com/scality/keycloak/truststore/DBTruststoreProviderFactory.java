8ad7f866b667a760d1fdb8a890625af08dd11e8e
Jean-Baptiste WATENBERG <jean-baptiste.watenberg@scality.com>
2024-06-25 11:39:16 +0200

    Enable support for hot reloading certificates

--- src/main/java/com/scality/keycloak/truststore/DBTruststoreProviderFactory.java
+++ src/main/java/com/scality/keycloak/truststore/DBTruststoreProviderFactory.java
@@ -3,25 +3,30 @@ package com.scality.keycloak.truststore;
 import org.keycloak.Config.Scope;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.KeycloakSessionFactory;
+import org.keycloak.quarkus.runtime.integration.QuarkusKeycloakSessionFactory;
 import org.keycloak.truststore.TruststoreProvider;
 import org.keycloak.truststore.TruststoreProviderFactory;
 
 public class DBTruststoreProviderFactory implements TruststoreProviderFactory {
 
+    private TruststoreProvider provider;
+
     @Override
     public TruststoreProvider create(KeycloakSession session) {
-        return new DBTruststoreProvider(session);
+        return provider;
     }
 
     @Override
     public void init(Scope config) {
         // Noop
-
     }
 
     @Override
     public void postInit(KeycloakSessionFactory factory) {
         // Noop
+        KeycloakSession session = factory.create();
+        provider = new DBTruststoreProvider(session);
+        TruststoreProviderSingleton.set(provider);
     }
 
     @Override
